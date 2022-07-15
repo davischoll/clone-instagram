ac42acfa1531d7aa51b20b0de144338f4781c9a7
Davi Scholl <davis@bandavide.com.br>
2022-07-14 23:25:40 -0300

    Project init

---
+++ README.md
@@ -0,0 +1,9 @@
+## Clone Instagram
+
+Projeto desenvolvido no curso "Ruby on Rails 6" - Lucas Caton.
+
+* Ruby 3.1.2
+
+* Rails 6.1.6
+
+* Postgresql
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,38 @@
+class PostsController < ApplicationController
+  include SuggestedUsers
+
+  before_action :set_post, only: %i[show]
+  before_action :set_suggested_users, only: %i[index]
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params.merge(created_by: current_user))
+
+    if @post.save
+      redirect_to @post, notice: "Post was successfully created."
+    else
+      flash.now[:alert] = @post.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:description)
+  end
+end
