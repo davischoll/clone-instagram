0056ad93d1a2fb485b3d7f5c80cd24ece080477f
Davi Scholl <davis@bandavide.com.br>
2022-08-20 19:25:32 -0300

    Add users avatar configuration

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def embedded_svg(path)
     File.open("app/assets/images/#{path}", "rb") { |file| raw(file.read) }
   end
+
+  def user_avatar(user)
+    image_tag user.avatar.attached? ? user.avatar : "default-avatar.jpg", class: "photo"
+  end
 end
